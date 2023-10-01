ce3485418e08e1baaf6c9c601eb0de895cff71eb
Sai Maneesh <saimaneesh1115@gmail.com>
2023-10-01 16:07:16 +0530

    returning custom responses

--- basic.js
+++ basic.js
@@ -1,5 +1,15 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => console.log("Maneesh"));
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("<html><h1>Welcome Home</h1></html>");
+    return res.end();
+  } else if (req.url === "/about") {
+    res.write("<html><h1>About Us</h1></html>");
+    return res.end();
+  } else if (req.url === "/node") {
+    res.write("<html><h1>Welcome to my Node Js project</h1></html>");
+  }
+});
 
 server.listen(8000);
