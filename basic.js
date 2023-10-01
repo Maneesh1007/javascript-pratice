569e57600994d567467ac1d6c8d998875fbb6b84
Sai Maneesh <saimaneesh1115@gmail.com>
2023-10-01 22:47:48 +0530

    using Middle wares

--- basic.js
+++ basic.js
@@ -1,14 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("i am in middleware");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title" /><input type="text" name="tag" /><button type="submit">Add</button></form>'
+  );
 });
 
-app.use((req, res, next) => {
-  res.send({ key: 1000 });
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello this is express Framework</h1>");
 });
 
 app.listen(8000);
