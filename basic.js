8b3e0512c06dc7bd9b4364342d99faa02b998ad5
Sai Maneesh <saimaneesh1115@gmail.com>
2023-10-01 17:48:25 +0530

    writing and reading from a file

--- basic.js
+++ basic.js
@@ -1,14 +1,37 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write("<html><h1>Welcome Home</h1></html>");
-    return res.end();
-  } else if (req.url === "/about") {
-    res.write("<html><h1>About Us</h1></html>");
-    return res.end();
-  } else if (req.url === "/node") {
-    res.write("<html><h1>Welcome to my Node Js project</h1></html>");
+  const url = req.url;
+
+  if (url === "/") {
+    fs.readFile("message.txt", { encoding: "utf8" }, (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      res.write("<html>");
+      res.write(`<body>${data}</body>`);
+      res.write("<form action='/message' method='POST'>");
+      res.write("<input type='text' name='message'>");
+      res.write("<button type='submit'>Submit</button>");
+      res.write("</form>");
+      res.write("</html>");
+      return res.end();
+    });
+  }
+
+  if (url === "/message" && req.method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => body.push(chunk));
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
   }
 });
 
