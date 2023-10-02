7b6e790bb14dae0be9a45e466420859aec460b93
Sai Maneesh <saimaneesh1115@gmail.com>
2023-10-02 08:36:14 +0530

    chat application

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const { AsyncLocalStorage } = require("async_hooks");
+
+const localStorage = new AsyncLocalStorage();
 
 const router = express.Router();
 
-router.get("/add-product", (req, res, next) => {
+router.use(bodyParser.urlencoded({ extended: false }));
+
+router.get("/login", (req, res, next) => {
   res.send(
-    '<form action="/product" method="POST"><input type="text" name="title" /><input type="text" name="tag" /><button type="submit">Add</button></form>'
+    '<form action="/storage" method="POST"><input type="text" name="username" /><button type="submit">Login</button></form>'
   );
 });
 
-router.post("/product", (req, res, next) => {
+router.post("/storage", (req, res, next) => {
   console.log(req.body);
 
   res.redirect("/");
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
 
 const router = express.Router();
 
+router.use(bodyParser.urlencoded({ extended: false }));
+
 router.get("/", (req, res, next) => {
-  res.send("<h1>Hello this is express Framework</h1>");
+  res.send(
+    '<form action="/message" method="POST"><input type = "text" name = "message" /><button type="submit">Send</button> </form>'
+  );
+});
+
+router.post("/message", (req, res, next) => {
+  fs.writeFile("username.txt", req.body, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/");
+    }
+  });
 });
 
 module.exports = router;
